f3da2b80caa7a06cdbe682b72ff89b5ac464471e
vikingviolinist <misopetrik@gmail.com>
2022-06-11 22:29:16 +0200

    🎨 Create constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const USER = Object.freeze({
+  FIRSTNAME: 'firstName',
+  LASTNAME: 'lastName',
+  BIRTHDAY: 'birthday',
+});
+
+export const ORDER = Object.freeze({
+  ASCENDING: 'ascending',
+  DESCENDING: 'descending',
+});
